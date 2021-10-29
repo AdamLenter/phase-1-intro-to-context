8ad2c57959ef0b5f987e52a5ea570af48dc42c35
Adam <adamlenter1@gmail.com>
2021-10-29 16:15:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function createEmployeeRecord(employeeRecord) {
+    const record = {
+        firstName: employeeRecord[0],  
+        familyName: employeeRecord[1],
+        title: employeeRecord[2], 
+        payPerHour: employeeRecord[3], 
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return record;
+}
+
+function createEmployeeRecords(employeeRecords) {
+    let allEmployeeRecords = [];
+
+    for(record of employeeRecords) {
+        allEmployeeRecords.push(createEmployeeRecord(record));
+    }
+
+return allEmployeeRecords;
+}
+
+
+const firstRecord = ["Adam", "Lenter", "King", 25];
+const secondRecord = ["Jamie", "Lenter", "Queen", 100];
+const ourRecords = [firstRecord, secondRecord];
+console.log(createEmployeeRecords(ourRecords));
